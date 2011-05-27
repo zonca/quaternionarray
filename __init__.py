fixed bug in rotation, missing angle/2

__init__.py
@@ -88,4 +88,8 @@ def nlerp(targettime, time, q):
 
 def rotation(axis, angle):
     """Rotation quaternions of angles [rad] around axes [already normalized]"""
-    return np.hstack([np.asarray(axis)*np.sin(angle)[:,None],np.cos(angle)[:,None]])
+    try:
+        angle = angle[:,None]
+    except:
+        pass
+    return np.hstack([np.asarray(axis)*np.sin(angle/2.),np.cos(angle/2.)])

test_quaternionarray.py
@@ -84,7 +84,7 @@ class TestQuaternionArray(unittest.TestCase):
 
     def test_rotation(self):
         np.testing.assert_array_almost_equal(
-            qarray.rotation(np.array([0,0,1]), np.radians(30)),         np.array([0, 0, 1/2., np.sqrt(3)/2]) 
+            qarray.rotation(np.array([0,0,1]), np.radians(30)),  np.array([0, 0, np.sin(np.radians(15)), np.cos(np.radians(15))])
             )
 
 if __name__ == '__main__':